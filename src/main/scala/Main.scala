a158a8769db5143671ebd1251b0243c86cc67d94
Mikhail Skorykh <mskorykh@evolution.com>
2023-04-03 00:40:16 +0400

    Add tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,46 +1,66 @@
 import scala.collection.immutable.List
 
 enum Conj:
-  case And, But, Or, Then
+  case And, But, Or, In, On, Of
 
 enum Root:
-  case Me, You, It, Food, Have, Water
+  case Me, You, It, Food, Have, Water, Strong, Good
 
 enum Word:
   case Unit(root: Root)
-  case Comb(word: Word, root: Root)
+  case Comb(root: Root, word: Word)
 
 enum Lex:
   case Unit(word: Word) // Subject
-  case Verb(lex: Lex, word: Word)
-  case Object(lex: Lex, word: Word)
-  case Adjective(lex: Lex, word: Word)
+  case Verb(word: Word, lex: Lex)
+  case Object(word: Word, lex: Lex)
+  case Adjective(word: Word, lex: Lex)
+  case Conjaction(left: Lex, conj: Conj, right: Lex)
 
 // Utils
+//  TODO: remove a lot of boilerplate
 
-// + (word comb)
-extension (a: Word)
-  def +(b: Root): Word = Word.Comb(a, b)
+// + (word combination)
 extension (a: Root)
-  def +(b: Root): Word = Word.Comb(Word.Unit(a), b)
+  def +(b: Root): Word = Word.Comb(a, Word.Unit(b))
 
 // ! (verb)
-extension (a: Lex)
-  def !(b: Word): Lex = Lex.Verb(a, b)
+extension (a: Root)
+  def !(b: Root): Lex = Lex.Verb(Word.Unit(a), Lex.Unit(Word.Unit(b)))
+  def !(b: Lex): Lex = Lex.Verb(Word.Unit(a), b)
 extension (a: Word)
-  def !(b: Word): Lex = Lex.Verb(Lex.Unit(a), b)
-extension (a: Word)
-  def !(b: Root): Lex = Lex.Verb(Lex.Unit(a), Word.Unit(b))
+  def !(b: Root): Lex = Lex.Verb(a, Lex.Unit(Word.Unit(b)))
+  def !(b: Lex): Lex = Lex.Verb(a, b)
 
 // > (direct object)
-extension (a: Lex)
-  def >(b: Word): Lex = Lex.Object(a, b)
-extension (a: Lex)
-  def >(b: Root): Lex = Lex.Object(a, Word.Unit(b))
+extension (a: Root)
+  def >(b: Root): Lex = Lex.Object(Word.Unit(a), Lex.Unit(Word.Unit(b)))
 extension (a: Word)
-  def >(b: Word): Lex = Lex.Object(Lex.Unit(a), b)
+  def >(b: Root): Lex = Lex.Object(a, Lex.Unit(Word.Unit(b)))
+  def >(b: Word): Lex = Lex.Object(a, Lex.Unit(b))
+
+// < (adjective)
+extension (a: Root)
+  def <(b: Root): Lex = Lex.Adjective(Word.Unit(a), Lex.Unit(Word.Unit(b)))
 extension (a: Word)
-  def >(b: Root): Lex = Lex.Object(Lex.Unit(a), Word.Unit(b))
+  def <(b: Root): Lex = Lex.Adjective(a, Lex.Unit(Word.Unit(b)))
+  def <(b: Word): Lex = Lex.Adjective(a, Lex.Unit(b))
+
+// Conjunction functions
+extension (left: Lex)
+  infix def and(right: Lex): Lex = Lex.Conjaction(left, Conj.And, right)
+  infix def or(right: Lex): Lex = Lex.Conjaction(left, Conj.Or, right)
+  infix def but(right: Lex): Lex = Lex.Conjaction(left, Conj.But, right)
+  infix def in(right: Lex): Lex = Lex.Conjaction(left, Conj.In, right)
+  infix def on(right: Lex): Lex = Lex.Conjaction(left, Conj.On, right)
+  infix def of(right: Lex): Lex = Lex.Conjaction(left, Conj.Of, right)
+
+extension (left: Root)
+  infix def and(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.And, right)
+  infix def or(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.Or, right)
+  infix def but(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.But, right)
+  infix def in(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.In, right)
+  infix def of(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.Of, right)
 
 // Render
 
@@ -52,7 +72,9 @@ given conjShow: Show[Conj] with
     case Conj.And => "AND"
     case Conj.But => "BUT"
     case Conj.Or => "OR"
-    case Conj.Then => "THEN"
+    case Conj.In => "IN"
+    case Conj.On => "ON"
+    case Conj.Of => "OF"
 
 given rootShow: Show[Root] with
   def show(r: Root): String = r match
@@ -62,33 +84,38 @@ given rootShow: Show[Root] with
     case Root.Food => "food"
     case Root.Have => "have"
     case Root.Water => "water"
+    case Root.Strong => "strong"
+    case Root.Good => "good"
 
 given showWord(using showRoot: Show[Root]): Show[Word] with
   def show(w: Word): String = w match
     case Word.Unit(root) => showRoot.show(root)
-    case Word.Comb(word, root) => s"${show(word)}-${showRoot.show(root)}"
+    case Word.Comb(root, word) => s"${showRoot.show(root)}-${show(word)}"
 
-given showLex(using showWord: Show[Word]): Show[Lex] with
+given showLex(using showWord: Show[Word], showConj: Show[Conj]): Show[Lex] with
+  val subject = "?"
   val verbMark = "i"
   val objectMark = "a"
   val adjectiveMark = "e"
 
   def show(l: Lex): String = l match
     case Lex.Unit(word) => showWord.show(word)
-    case Lex.Verb(lex, word) => s"${show(lex)} $verbMark ${showWord.show(word)}"
-    case Lex.Object(lex, word) => s"${show(lex)} $objectMark ${showWord.show(word)}"
-    case Lex.Adjective(lex, word) => s"${show(lex)} $adjectiveMark ${showWord.show(word)}"
+    case Lex.Verb(word, lex) => s"${showWord.show(word)} $verbMark ${show(lex)}"
+    case Lex.Object(word, lex) => s"${showWord.show(word)} $objectMark ${show(lex)}"
+    case Lex.Adjective(word, lex) => s"${showWord.show(word)} $adjectiveMark ${show(lex)}"
+    case Lex.Conjaction(left, conj, right) => s"${show(left)} ${showConj.show(conj)} ${show(right)}"
 
 @main def main(): Unit =
   given food: Root = Root.Food
   given me: Root = Root.Me
   given have: Root = Root.Have
   given it: Root = Root.It
+  given water: Root = Root.Water
 
   println(
-    // showLex.show(
-      (food + me) ! (have + it) > it
-    // )
+    showLex.show(
+      food ! have > water
+    )
   )
 end main
 
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,44 +1,88 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("Phrase to string") {
-    assertNoDiff(
-      Phrase(
-        Lex.Subject(Word.Ad(Root.Mi, Word.Unit(Root.Manja))),
-        Lex.Verb(Word.Unit(Root.Ave)),
-        Lex.Object(Word.Ad(Root.Tu, Word.Unit(Root.Vasa))),
-      ).toString(),
-      "manja mi i ave a vasa tu",
-    )
+  test("water is good") {
+    given water: Root = Root.Water
+    given good: Root = Root.Good
 
     assertNoDiff(
-      Phrase(
-        Lex.Subject(Word.Ad(Root.Ave, Word.Unit(Root.Vasa))),
-        Lex.Adjective(Word.Unit(Root.Manja)),
-      ).toString(),
-      "vasa ave e manja",
+      showLex.show(
+        water < good
+      ),
+      "water e good",
     )
   }
 
-  test("with conj") {
+  test("a food has water") {
+    given food: Root = Root.Food
+    given have: Root = Root.Have
+    given water: Root = Root.Water
+
     assertNoDiff(
-      Sentence(
-        Phrase(
-          Lex.Subject(Word.Con(
-            Word.Unit(Root.Mi),
-            Conj.An,
-            Word.Unit(Root.Tu),
-          )),
-          Lex.Verb(Word.Unit(Root.Ave)),
-          Lex.Object(Word.Unit(Root.Manja))
-        ),
-        Conj.Pero,
-        Phrase(
-          Lex.Subject(Word.Unit(Root.Si)),
-          Lex.Adjective(Word.Unit(Root.Vasa))
-        ),
-      ).toString(),
-      "mi AN tu i ave a manja PERO si e vasa"
+      showLex.show(
+        food ! have > water
+      ),
+      "food i have a water",
+    )
+  }
+
+  test("my food strongly has good water") {
+    given food: Root = Root.Food
+    given me: Root = Root.Me
+    given have: Root = Root.Have
+    given strong: Root = Root.Strong
+    given water: Root = Root.Water
+    given good: Root = Root.Good
+
+    assertNoDiff(
+      showLex.show(
+        food+me ! have+strong > water+good
+      ),
+      "food-me i have-strong a water-good",
+    )
+  }
+
+  test("food has water and food has water") {
+    given food: Root = Root.Food
+    given me: Root = Root.Me
+    given have: Root = Root.Have
+    given strong: Root = Root.Strong
+    given water: Root = Root.Water
+    given good: Root = Root.Good
+
+    assertNoDiff(
+      showLex.show(
+        food ! have > water `and` food ! have > water
+      ),
+      "food i have a water AND food i have a water",
+    )
+  }
+
+  test("you and me are good") {
+    given me: Root = Root.Me
+    given you: Root = Root.You
+    given good: Root = Root.Good
+
+    assertNoDiff(
+      showLex.show(
+        me `and` you < good
+      ),
+      "me AND you e good",
+    )
+  }
+
+  test("I have and make better a food") {
+    given me: Root = Root.Me
+    given have: Root = Root.Have
+    given good: Root = Root.Good
+    given food: Root = Root.Food
+
+    assertNoDiff(
+      showLex.show(
+        me ! have `and` good > food
+      ),
+      "me i have AND good a food",
     )
   }
 }
+
