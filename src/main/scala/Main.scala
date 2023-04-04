12955c3c14f41e64adcc436d026dfeffa888b8cd
Mikhail Skorykh <mskorykh@evolution.com>
2023-04-04 10:53:20 +0400

    Rewrite to more generic and parsing-first approach

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,121 +1,79 @@
-import scala.collection.immutable.List
+enum Lex[Root, Marker, Conj]:
+  case End()
+  case Word(root: Root)
+  case Conj(conj: Conj, lex: Lex[Root, Marker, Conj])
+  case Phrase(marker: Marker, word: Word[Root, Marker, Conj], lex: Lex[Root, Marker, Conj])
 
-enum Conj:
-  case And, But, Or, In, On, Of
+trait Parsable[A]:
+  val values: Array[String]
+  def show(a: A): String
+  def unshow(a: String): A
 
 enum Root:
-  case Me, You, It, Food, Have, Water, Strong, Good
+  case Mi, You, Love, Big, Not
 
-enum Word:
-  case Unit(root: Root)
-  case Comb(root: Root, word: Word)
+enum Conj:
+  case And, Or, Then
 
-enum Lex:
-  case Unit(word: Word) // Subject
-  case Verb(word: Word, lex: Lex)
-  case Object(word: Word, lex: Lex)
-  case Adjective(word: Word, lex: Lex)
-  case Conjaction(left: Lex, conj: Conj, right: Lex)
+enum Marker:
+  case Object, Subject, Verb, Adjective
 
-// Utils
-//  TODO: remove a lot of boilerplate
+object RootV extends Parsable[Root]:
+  val values: Array[String] = Array(Root.Mi, Root.You, Root.Love, Root.Big, Root.Not).map(show)
 
-// + (word combination)
-extension (a: Root)
-  def +(b: Root): Word = Word.Comb(a, Word.Unit(b))
+  def unshow(a: String) = a match
+    case "mi" => Root.Mi
+    case "you" => Root.You
+    case "love" => Root.Love
+    case "big" => Root.Big
+    case _ => Root.Not
 
-// ! (verb)
-extension (a: Root)
-  def !(b: Root): Lex = Lex.Verb(Word.Unit(a), Lex.Unit(Word.Unit(b)))
-  def !(b: Lex): Lex = Lex.Verb(Word.Unit(a), b)
-extension (a: Word)
-  def !(b: Root): Lex = Lex.Verb(a, Lex.Unit(Word.Unit(b)))
-  def !(b: Lex): Lex = Lex.Verb(a, b)
-
-// > (direct object)
-extension (a: Root)
-  def >(b: Root): Lex = Lex.Object(Word.Unit(a), Lex.Unit(Word.Unit(b)))
-extension (a: Word)
-  def >(b: Root): Lex = Lex.Object(a, Lex.Unit(Word.Unit(b)))
-  def >(b: Word): Lex = Lex.Object(a, Lex.Unit(b))
-
-// < (adjective)
-extension (a: Root)
-  def <(b: Root): Lex = Lex.Adjective(Word.Unit(a), Lex.Unit(Word.Unit(b)))
-extension (a: Word)
-  def <(b: Root): Lex = Lex.Adjective(a, Lex.Unit(Word.Unit(b)))
-  def <(b: Word): Lex = Lex.Adjective(a, Lex.Unit(b))
-
-// Conjunction functions
-extension (left: Lex)
-  infix def and(right: Lex): Lex = Lex.Conjaction(left, Conj.And, right)
-  infix def or(right: Lex): Lex = Lex.Conjaction(left, Conj.Or, right)
-  infix def but(right: Lex): Lex = Lex.Conjaction(left, Conj.But, right)
-  infix def in(right: Lex): Lex = Lex.Conjaction(left, Conj.In, right)
-  infix def on(right: Lex): Lex = Lex.Conjaction(left, Conj.On, right)
-  infix def of(right: Lex): Lex = Lex.Conjaction(left, Conj.Of, right)
-
-extension (left: Root)
-  infix def and(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.And, right)
-  infix def or(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.Or, right)
-  infix def but(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.But, right)
-  infix def in(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.In, right)
-  infix def of(right: Lex): Lex = Lex.Conjaction(Lex.Unit(Word.Unit(left)), Conj.Of, right)
-
-// Render
-
-trait Show[A]:
-  def show(a: A): String
-
-given conjShow: Show[Conj] with
-  def show(c: Conj): String = c match
-    case Conj.And => "AND"
-    case Conj.But => "BUT"
-    case Conj.Or => "OR"
-    case Conj.In => "IN"
-    case Conj.On => "ON"
-    case Conj.Of => "OF"
-
-given rootShow: Show[Root] with
-  def show(r: Root): String = r match
-    case Root.Me => "me"
+  def show(a: Root): String = a match
+    case Root.Mi => "mi"
     case Root.You => "you"
-    case Root.It => "it"
-    case Root.Food => "food"
-    case Root.Have => "have"
-    case Root.Water => "water"
-    case Root.Strong => "strong"
-    case Root.Good => "good"
+    case Root.Love => "love"
+    case Root.Big => "big"
+    case Root.Not => "not"
 
-given showWord(using showRoot: Show[Root]): Show[Word] with
-  def show(w: Word): String = w match
-    case Word.Unit(root) => showRoot.show(root)
-    case Word.Comb(root, word) => s"${showRoot.show(root)}-${show(word)}"
+object ConjV extends Parsable[Conj]:
+  val values = Array(Conj.And, Conj.Or, Conj.Then).map(show)
 
-given showLex(using showWord: Show[Word], showConj: Show[Conj]): Show[Lex] with
-  val subject = "?"
-  val verbMark = "i"
-  val objectMark = "a"
-  val adjectiveMark = "e"
+  def unshow(a: String) = a match
+    case "and" => Conj.And
+    case "or" => Conj.Or
+    case _ => Conj.Then
 
-  def show(l: Lex): String = l match
-    case Lex.Unit(word) => showWord.show(word)
-    case Lex.Verb(word, lex) => s"${showWord.show(word)} $verbMark ${show(lex)}"
-    case Lex.Object(word, lex) => s"${showWord.show(word)} $objectMark ${show(lex)}"
-    case Lex.Adjective(word, lex) => s"${showWord.show(word)} $adjectiveMark ${show(lex)}"
-    case Lex.Conjaction(left, conj, right) => s"${show(left)} ${showConj.show(conj)} ${show(right)}"
+  def show(a: Conj): String = a match
+    case Conj.And => "and"
+    case Conj.Or => "or"
+    case Conj.Then => "then"
+
+object MarkerV extends Parsable[Marker]:
+  val values = Array(Marker.Object, Marker.Subject, Marker.Verb, Marker.Adjective).map(show)
+
+  def unshow(a: String): Marker = a match
+    case "o" => Marker.Object 
+    case "a" => Marker.Subject 
+    case "i" => Marker.Verb 
+    case "e" => Marker.Adjective
+
+  def show(a: Marker): String = a match
+    case Marker.Object => "a"
+    case Marker.Subject => "o"
+    case Marker.Verb => "i"
+    case Marker.Adjective => "e"
+
+def parse(
+  input: List[String],
+): Lex[Root, Marker, Conj] =
+  input match
+    case mark :: word :: tail if MarkerV.values.contains(mark) =>
+      Lex.Phrase(MarkerV.unshow(mark), Lex.Word(RootV.unshow(word)), parse(tail))
+    case mark :: tail if ConjV.values.contains(mark) =>
+      Lex.Conj(ConjV.unshow(mark), parse(tail))
+    case word :: tail => Lex.Word(RootV.unshow(word))
+    case Nil => Lex.End()
 
 @main def main(): Unit =
-  given food: Root = Root.Food
-  given me: Root = Root.Me
-  given have: Root = Root.Have
-  given it: Root = Root.It
-  given water: Root = Root.Water
-
-  println(
-    showLex.show(
-      food ! have > water
-    )
-  )
+  println(parse("o mi i love a cat".split(" ").toList))
 end main
-
